5d2a89fe86c315f493ebaa5cb19239a4c63b1669
Rahul Tyagi <tyagi8285@gmail.com>
2021-06-07 12:55:46 +0530

    add user controller and home page title change

---
+++ FirstWebApp/Controllers/User_InfoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FirstWebApp.Models;
+
+namespace FirstWebApp.Controllers
+{
+    public class User_InfoController : Controller
+    {
+        private LinkedinEntities db = new LinkedinEntities();
+
+        // GET: User_Info
+        public ActionResult Index()
+        {
+            return View(db.User_Info.ToList());
+        }
+
+        // GET: User_Info/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User_Info user_Info = db.User_Info.Find(id);
+            if (user_Info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_Info);
+        }
+
+        // GET: User_Info/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User_Info/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,Name,Email,Password,Mobile,Country,FilePath,CretaedDate,IsStatus")] User_Info user_Info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User_Info.Add(user_Info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user_Info);
+        }
+
+        // GET: User_Info/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User_Info user_Info = db.User_Info.Find(id);
+            if (user_Info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_Info);
+        }
+
+        // POST: User_Info/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,Name,Email,Password,Mobile,Country,FilePath,CretaedDate,IsStatus")] User_Info user_Info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user_Info).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user_Info);
+        }
+
+        // GET: User_Info/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User_Info user_Info = db.User_Info.Find(id);
+            if (user_Info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_Info);
+        }
+
+        // POST: User_Info/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User_Info user_Info = db.User_Info.Find(id);
+            db.User_Info.Remove(user_Info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
